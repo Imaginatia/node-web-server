129d888f67974b822ae30b69e3fa90fd45a7dbdd
Zhenye <492684811@qq.com>
2018-04-02 17:20:52 -0400

    Project page added, various pages modified

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 var app = express();
 
 const port = process.env.PORT || 3000;
@@ -9,22 +10,28 @@ app.set('view engine', 'hbs');
 
 app.use((req, res, next) => {
   var now = new Date().toString();
-  console.log(`${now}, ${req.method}, ${req.url}`);
+  var log = `${now}, ${req.method}, ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append to file');
+    }
+  });
   next();
 });
 
-app.use((req, res, next) => {
-  res.render('maintenance.hbs', {
-    pageTitle: 'Maintenance',
-    message: 'We wll come back later'
-  })
-});
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs', {
+//     pageTitle: 'Maintenance',
+//     message: 'We will come back later'
+//   })
+// });
 
 app.use(express.static(__dirname + '/public'));
 
 
 hbs.registerHelper('getCurrentYear', () => new Date().getFullYear());
-hbs.registerHelper('screamIt', (text) => text.toUpperCase())
+hbs.registerHelper('screamIt', (text) => text.toUpperCase());
 
 app.get('/', (req, res) => {
   res.render('home.hbs', {
@@ -33,16 +40,22 @@ app.get('/', (req, res) => {
   });
 });
 
-app.get('/bad', (req, res) => {
-  res.send('Error!');
-})
-
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
     pageTitle: 'About page'
   })
-})
+});
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects page',
+    message: 'This is projects page'
+  })
+});
+
+app.get('/bad', (req, res) => {
+  res.send('Error!');
+});
 
 app.listen(port, () => {
   console.log(`Server is on port ${port}`);
